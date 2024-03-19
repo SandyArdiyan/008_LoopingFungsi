08b9f10ce852f3b635610a699ec0799ddfeabed6
SandyArdiyan <sanardiyan881@gmail.com>
2024-03-19 20:50:07 +0700

    Update

---
+++ Contoh3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{ 
+    int i;
+    string nama[5];
+
+    for (i =150; > 100; i-=10)
+    {
+        cout << i << "."<< "SELAMAT BERBUKA PUASA" << endl;
+
+    }
+    cout << "Nilai i terakhiran = "<< i << endl;
+
+    for (i = 0; i <5; i++) {
+        Cout << "Masukan nama ke-" << i << ":";
+    }
+
+
+    for (i = 0; i < 5; i ++){
+            Cout<< "Nama ke-" << i << ":" << nama[i] << endl;
+    }
